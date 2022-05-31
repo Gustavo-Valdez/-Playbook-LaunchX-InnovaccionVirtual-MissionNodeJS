5bc0a875cd07344d0b73039bf7a0d31f1caf798a
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-05-31 15:18:25 -0500

    To show my commits on GitHub

---
+++ weekly_mission_2/example5/pokemon.js
@@ -0,0 +1,8 @@
+/*
+Este ejemplo incluye dependencias. En tu línea de comando ve a este proyecto y ejecuta el comando `npm install` para descargar las dependencias, esto creará una carpeta node_modules
+*/
+export default class Pokemon {
+  constructor(name) {
+    this.name = name;
+  }
+}
