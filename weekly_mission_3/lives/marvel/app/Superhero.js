211518316cc1c2ed1ae4b4cae1bb7c40f1b3e1c1
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-06-01 19:47:46 -0500

    Agregando clase y su prueba de unidad

---
+++ weekly_mission_3/lives/marvel/app/Superhero.js
@@ -0,0 +1,9 @@
+class Superhero { 
+    constructor(name, heroName, actor) {
+        this.name = name
+        this.heroname = heroName
+        this.actor = actor
+    }
+}
+
+module.exports = Superhero
--- weekly_mission_3/lives/marvel/test/Superhero.test.js
+++ weekly_mission_3/lives/marvel/test/Superhero.test.js
@@ -1,6 +1,12 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18)
+const Superhero = require("./../app/Superhero");
+
+describe("Unit Test for Superhero", () => {
+    test('Case 1: Get a superhero', () => {
+
+        const ironman = new Superhero("Iron Man","Tony Stark", "Robert Downey Jr.")
+
+        expect(ironman.name).toBe("Iron Man")
+        expect(ironman.actor).toBe("Robert Downey Jr.")
+        expect(ironman.heroname).toBe("Tony Stark")
     });
 })
