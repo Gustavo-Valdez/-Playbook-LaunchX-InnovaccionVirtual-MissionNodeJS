5ecf01db8b9d03f9bee346c9235c82a2b73b180c
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-06-02 21:14:45 -0500

    Agregando a clase User su respectiva prueba

---
+++ weekly_mission_3/TWITTER/app/models/User.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(id, username, name, bio, dateCreated, lastUpdated) {
+        this.id = id
+        this.username = username
+        this.name = bio
+        this.dateCreated = dateCreated
+        this.lastUpdate = lastUpdated
+    }
+
+}
+
+module.exports = User
